12c22da12e144d4b93570a0133859b6d1f7324d1
arbhosle20 <arbhosle20@gmail.com>
2023-07-23 13:50:45 +0530

    all

---
+++ models.py
@@ -0,0 +1,11 @@
+from database import Base
+from sqlalchemy import Column, Integer, String, Boolean
+
+class mysbi(Base):
+    __tablename__ = 'todos'
+
+    id= Column(Integer, primary_key=True, index=True)
+    title = Column(String(255))
+    description = Column(String(255))
+    priority = Column(Integer)
+    complete = Column(Boolean, default=False)
